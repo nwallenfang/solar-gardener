b74d016056a75f8371d238fd159e5aaf881b7800
nwallenfang <nils@wallenfang.de>
2022-11-05 10:39:32 +0100

    camera scene

--- Logic/Player/InterpolatedCamera.gd
+++ Logic/Player/InterpolatedCamera.gd
@@ -8,17 +8,16 @@ func _ready() -> void:
 	Game.camera = self
 	Game.multitool = $ObjectUI/Multitool
 	yield(get_tree(), "idle_frame")
-	head = Game.player.get_node("Head")
+	head = Game.player.get_node("Head/Camera")
 
 func new_player_set():
-	head = Game.player.get_node("Head")
+	head = Game.player.get_node("Head/Camera")
 
 var interpolated_target: Vector3
-var lerping_accel = 20.0
+var lerping_accel = 30.0
 func _process(delta: float) -> void:
 	if is_instance_valid(head):
 		var target_transform: Transform = head.get_global_transform_interpolated()
-		target_transform = target_transform.translated(2.0 * Vector3.LEFT)
 		target_transform.origin =  lerp(interpolated_target, target_transform.origin, min(lerping_accel * delta, 1.0))
 		self.global_transform = target_transform
 		interpolated_target = target_transform.origin
