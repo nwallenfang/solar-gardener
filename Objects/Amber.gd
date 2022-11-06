8d09f40894307102fa3c12260b07792a0689ca58
nheeb <nitai64@yahoo.de>
2022-11-06 18:29:06 +0100

    Soil Analyse

--- Objects/Amber.gd
+++ Objects/Amber.gd
@@ -8,7 +8,7 @@ const FLYING_PICKUP = preload("res://Objects/FlyingPickup.tscn")
 func on_analyse():
 	var pickup = FLYING_PICKUP.instance()
 	Game.planet.add_child(pickup)
-	pickup.global_transform.basis = Utility.get_basis_y_alligned(Game.planet.global_translation.direction_to(self.global_translation))
+	pickup.global_transform.basis = Utility.get_basis_y_aligned(Game.planet.global_translation.direction_to(self.global_translation))
 	pickup.global_translation = self.global_translation
 	pickup.setup_as_seed(seed_name)
 	# TODO Seed count?
